4dce1067d177a864fbaa677f3bf9529855231b27
saiyyadimran <imran.mitm@gmail.com>
2021-04-08 23:44:32 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -8,7 +8,7 @@ def call(Map args) {
 		cd `cat /tmp/filepath.txt`
 		pwd
 	"""
-	app = docker.build("${args.remote}/${args.project}/${args.imageName}:${args.tag}", "${args.otherargs} --file ${args.filePath} .")
+	app = docker.build("${args.remote}/${args.project}/${args.imageName}:${args.tag}", "${args.otherargs} --file ${args.filePath} ${args.filePath}")
 	echo "Image tagged"
 	docker.withRegistry("${GCR_URL}", "${GOOGLE_PROJECT_CRED_ID}") {
 		app.push()
