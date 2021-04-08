71892835ee50853521bd21dd4ff1714bbddf9941
saiyyadimran <imran.mitm@gmail.com>
2021-04-08 23:31:14 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -2,6 +2,10 @@ def call(Map args) {
 	sh """
 		pwd
 		cd `cat /tmp/filepath.txt`
+		ls
+		cd target
+		ls
+		cd `cat /tmp/filepath.txt`
 		pwd
 	"""
 	app = docker.build("${args.remote}/${args.project}/${args.imageName}:${args.tag}", "${args.otherargs} --file ${args.filePath} .")
